47a9baf062f96cf25eac8ee6f0d76082f88cd9c0
prince12m <prince.malonga12@gmail.com>
2022-02-03 02:06:50 +0000

    Now I have to block out the levels and dat since I now know how to set up a new scene (level)
    Signed-off-by: prince12m <prince.malonga12@gmail.com>

--- New Unity Project/Assets/Scripts/LevelLoader.cs
+++ New Unity Project/Assets/Scripts/LevelLoader.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
     public int iLevelToLoad;
-    public string slevelToLoad;
+    public string sLevelToLoad;
 
     public bool useIntegerToLoadLevel = false;
 
+    // Start is called before the first frame update
     void Start()
     {
         
     }
-
 
     // Update is called once per frame
     void Update()
@@ -22,17 +22,25 @@ public class LevelLoader : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject collisionGameObject = collision.gameobject;
+        GameObject collisionGameObject = collision.gameObject;
+
         if(collisionGameObject.name == "Player")
         {
-            LoadScene():
+            LoadScene();
         }
     }
 
     void LoadScene()
     {
-
+        if (useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
     }
 }
